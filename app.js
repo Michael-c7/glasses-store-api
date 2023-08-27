d320849ae2b7f00592afbaefe64771bb238bfc55
Michael Carr <mcarr2468@gmail.com>
2023-08-26 19:10:12 -0500

    add basic route stuff and mongoDb in package.json

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log('04 Store API')
+require("dotenv").config()
+
+const express = require("express")
+const app = express()
+
+const connectDB = require("./db/connect")
+const productsRouter = require("./routes/products")
+
+const notFoundMiddleware = require("./middleware/not-found")
+const errorMiddleware = require("./middleware/error-handler")
+
+
+// middleware
+app.use(express.json())
+
+
+// routes
+app.get("/", (req, res) => {
+    res.send("<h1>store api</h1><a href='/api/v1/products'>products route</a>")
+})
+
+app.use("/api/v1/products", productsRouter)
+
+
+// products route
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        // connect to db
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server @ ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
