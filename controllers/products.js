779a21d1c71e616b594e3320581ba4f2c3113bb2
Michael Carr <mcarr2468@gmail.com>
2023-08-28 16:24:40 -0500

    add more filters

--- controllers/products.js
+++ controllers/products.js
@@ -34,24 +34,51 @@ const getAllProducts = async (req, res) => {
     }
 
 
-    let result = Product.find(queryObject)
-
-    if(sort) {
-        const sortList = sort.split(",").join(" ")
-        result = result.sort(sortList)
-        // products = products.sort()
-        console.log(sort)
-    } else {
-        result = result.sort("name")
-    }
+    if (numericFilters) {
+        const operatorMap = {
+          ">": "$gt",
+          ">=": "$gte",
+          "=": "$eq",
+          "<": "$lt",
+          "<=": "$lte",
+        };
+        const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+        let filters = numericFilters.replace(
+          regEx,
+          (match) => `-${operatorMap[match]}-`
+        );
+        const options = ["price", "rating, reviews"];
+        filters = filters.split(",").forEach((item) => {
+          const [field, operator, value] = item.split("-");
+          if (options.includes(field)) {
+            queryObject[field] = { [operator]: Number(value) };
+          }
+        });
+      }
     
-    // console.log(queryObject)
-    const products = await result
-    // const products = await Product.find(req.query)
-    // const products = await Product.find({
-    //     name: { $regex: name, $options: "i" }
-    //  })
-    res.status(200).json({ products })
+      let result = Product.find(queryObject);
+      // sort
+      if (sort) {
+        const sortList = sort.split(",").join(" ");
+        result = result.sort(sortList);
+      } else {
+        result = result.sort("name");
+      }
+    
+      if (fields) {
+        const fieldsList = fields.split(",").join(" ");
+        result = result.select(fieldsList);
+      }
+      const page = Number(req.query.page) || 1;
+      const limit = Number(req.query.limit) || 10;
+      const skip = (page - 1) * limit;
+    
+      result = result.skip(skip).limit(limit);
+      // 23
+      // 4 7 7 7 2
+    
+      const products = await result;
+      res.status(200).json({ products, productAmt: products.length });
 }
 
 
