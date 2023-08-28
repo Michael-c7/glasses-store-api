e8acca816128907a8d0ebf9d87878e0b40b2691a
Michael Carr <mcarr2468@gmail.com>
2023-08-28 16:10:21 -0500

    worked on sort functionality

--- controllers/products.js
+++ controllers/products.js
@@ -4,13 +4,36 @@ const Product = require("../models/product")
 
 const getAllProductsStatic = async (req, res) => {
     // throw new Error("testing async errors")
-    const products = await Product.find({ gender:"male", colors:"black"})
-    res.status(200).json({ msg: products })
+    const search = "ab"
+    const products = await Product.find({}).sort("-name price")
+    res.status(200).json({ products })
 }
 
 const getAllProducts = async (req, res) => {
-    console.log(req.query)
-    const products = await Product.find(req.query)
+    const { name, sort } = req.query;
+    const queryObject = {}
+    
+    if (name) {
+        queryObject.name = { $regex: name, $options: "i" }
+    }
+
+    let result = Product.find(queryObject)
+
+    if(sort) {
+        const sortList = sort.split(",").join(" ")
+        result = result.sort(sortList)
+        // products = products.sort()
+        console.log(sort)
+    } else {
+        result = result.sort("name")
+    }
+    
+    // console.log(queryObject)
+    const products = await result
+    // const products = await Product.find(req.query)
+    // const products = await Product.find({
+    //     name: { $regex: name, $options: "i" }
+    //  })
     res.status(200).json({ products })
 }
 
